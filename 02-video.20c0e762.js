869e937136e00b30f788b61574fa3b36a8adfc57
draumkona <107296655+draumkona@users.noreply.github.com>
2022-11-16 15:44:57 +0000

    Deploying to gh-pages from @ draumkona/goit-js-hw-08@f17092ea6d33e1e8f2b53420d4584c0907e89e1c 🚀

---
+++ 02-video.20c0e762.js
@@ -0,0 +1,2 @@
+document.querySelector("#vimeo-player");
+//# sourceMappingURL=02-video.20c0e762.js.map
---
+++ 02-video.4ca0694b.js
@@ -0,0 +1,2 @@
+document.querySelector("#vimeo-player");
+//# sourceMappingURL=02-video.4ca0694b.js.map
